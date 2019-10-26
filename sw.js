2c5d6a984b0b23ef222395ff00e50f7e7874b173
dsewnr <dsewnr@gmail.com>
2019-10-26 21:21:21 +0800

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-f3ac9f9acd666d43c073.js"
+    "url": "webpack-runtime-14066418a58ff3873761.js"
   },
   {
     "url": "styles.1812f792dc7f28de35af.css"
@@ -42,7 +42,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d7f5f2f05cd7086d9431ced381dc4a53"
+    "revision": "0122972d0279422c5a0998a318e68bb3"
   },
   {
     "url": "component---src-pages-404-js-665a0d3cb65fa6a253d6.js"
